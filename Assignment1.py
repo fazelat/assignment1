3339e616c9dc7ceb5d7e28b8c912f5896dc7cc59
fazelat <144057758+fazelat@users.noreply.github.com>
2023-09-04 23:40:37 +0500

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,383 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print(123)
+
+
+# In[2]:
+
+
+print(98.6)
+
+
+# In[3]:
+
+
+print("Hello World")
+
+
+# In[4]:
+
+
+x=12.2
+y=14
+x=100
+
+
+# In[5]:
+
+
+x1q3z9ocd=35.0
+x1q3z9afd=12.50
+x1q3p9afd=x1q3z9ocd*x1q3z9afd
+print(x1q3p9afd)
+
+
+# In[6]:
+
+
+a=35.0
+b=12.50
+c=a*b
+print(c)
+
+
+# In[8]:
+
+
+hours=35.0
+rate=12.50
+pay=hours*rate
+print(pay)
+
+
+# In[9]:
+
+
+x=2
+x=x+2
+print(x)
+
+
+# In[11]:
+
+
+x=3.9*x*(1-x)
+
+
+# In[14]:
+
+
+x=3.9*0.6*(1-0.6)
+
+
+# In[13]:
+
+
+xx=2
+xx=xx+2
+print(xx)
+
+
+# In[15]:
+
+
+yy=440*12
+print(yy)
+
+
+# In[16]:
+
+
+zz=yy/1000
+print(zz)
+
+
+# In[17]:
+
+
+jj=23
+kk=jj%5
+print(kk)
+
+
+# In[23]:
+
+
+print(4**3)
+
+
+# 
+
+# In[24]:
+
+
+x=1+2*3-4/5**6
+print(x)
+
+
+# In[25]:
+
+
+x=1+2**3/4*5
+print(x)
+
+
+# In[26]:
+
+
+ddd=1+4
+print(ddd)
+
+
+# In[28]:
+
+
+eee='hello '+'there'
+print(eee)
+
+
+# In[29]:
+
+
+eee='hello '+'there'
+eee=eee+1
+
+
+# In[30]:
+
+
+type(eee)
+
+
+# In[31]:
+
+
+type('hello')
+
+
+# In[32]:
+
+
+type(1)
+
+
+# In[33]:
+
+
+xx=1
+type(xx)
+
+
+# In[34]:
+
+
+temp=98.6
+type(temp)
+
+
+# In[35]:
+
+
+type(1)
+
+
+# In[36]:
+
+
+type(1.0)
+
+
+# In[37]:
+
+
+print(float(99)+100)
+
+
+# In[38]:
+
+
+i=42
+type(i)
+
+
+# In[40]:
+
+
+f=float(i)
+print(f)
+
+
+# In[41]:
+
+
+type(f)
+
+
+# In[42]:
+
+
+print(10/2)
+
+
+# In[43]:
+
+
+print(9/2)
+
+
+# In[44]:
+
+
+print(99/100)
+
+
+# In[45]:
+
+
+print(10.0/2.0)
+
+
+# In[46]:
+
+
+print(99.0/100.0)
+
+
+# In[48]:
+
+
+sval='123'
+type(sval)
+
+
+# In[49]:
+
+
+print(sval+1)
+
+
+# In[51]:
+
+
+ival=int(sval)
+type(ival)
+
+
+# In[52]:
+
+
+print(ival+1)
+
+
+# In[53]:
+
+
+nsv='hello bob
+
+niv=int(nsv)
+
+
+# In[1]:
+
+
+nam=input('Who are you? ')
+print('Welcome', nam)
+
+
+# In[55]:
+
+
+inp=input('Éuorpe floor')
+usf=int(inp)+1
+print('US floor',usf)
+
+
+# 
+
+# In[ ]:
+
+
+name=input("Enter file:")
+handle=open(name,'r')
+
+
+# In[ ]:
+
+
+counts=dict()
+for line in handle:
+    words=line.split()
+    for word in words:
+        counts[word]=counts.get(word,0)+1
+
+
+# In[ ]:
+
+
+bigcount=None
+bigword=None
+for word,couny in counts.items():
+    if bigcount is None or count>bigcount:
+        bigword=word
+        bigcount=count
+        print(bigword,bigcount)
+
+
+# In[2]:
+
+
+hours = float(input("Enter Hours: "))
+rate = float(input("Enter Rate: "))
+
+# Calc35ulate gross pay
+gross_pay = hours * rate
+
+# Display the result
+print("Pay:", gross_pay)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
